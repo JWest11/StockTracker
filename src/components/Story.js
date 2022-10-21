d49148c354b9be5e97bc0d6eaf1db3485df58125
JWest11 <114183436+JWest11@users.noreply.github.com>
2022-10-21 02:30:58 -0600

    responsive news component

--- src/components/Story.js
+++ src/components/Story.js
@@ -6,15 +6,15 @@ export default function Story({i, news}) {
 
   return (
     (i < news.length ? 
-    <div className="col p-4 border border-1">
+    <div className="col p-4 border border-1 newsStoryContainer">
         <h4>{news[i].headline} <span className="text-secondary newsSourceText">{news[i].source}</span></h4>
         <div className='row pt-2 pb-2 align-items-center'>
-            <div className="col"><p>{news[i].summary}</p></div>
-            <div className="col">
+            <div className="col newsCol"><p>{news[i].summary}</p></div>
+            <div className="col newsCol">
                 <img className="newsImage" src={news[i].image ? news[i].image : imgSrc}></img>
             </div>
         </div>
-        <a target="blank" className="btn btn-light border" href={news[i].url}>View Story</a>                    
+        <a target="blank" className="btn btn-secondary border" href={news[i].url}>View Story</a>                    
     </div>
     :
     <div></div>
